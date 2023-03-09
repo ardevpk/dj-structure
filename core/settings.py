32fe6c8b5f099aeef38d2938e3a95f7b6902a3ca
MudassarSharifCB <mudassar.sharif@crowdbotics.com>
2023-03-10 00:21:21 +0500

    updated

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -35,13 +36,20 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-
-	# Apps
-	'app.apps.AppConfig',
-
-	# Third party apps
-
 ]
+
+# DJANGO_CODE_APPS = [
+#     'school',
+#     'album'
+# ]
+
+# THIRD_PARTY_APPS = [
+#     'rest_framework',
+#     'django_filters',
+#     'corsheaders',
+# ]
+
+# INSTALLED_APPS += THIRD_PARTY_APPS + DJANGO_CODE_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -78,9 +86,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": config("DB_ENGINE"),
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT"),
     }
 }
 
@@ -140,3 +152,55 @@ LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'signin'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# AUTH_USER_MODEL = "users.User"
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        # "rest_framework.authentichttp://127.0.0.1:8000/
+    ),
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.OrderingFilter",
+    ],
+    "DEFAULT_PAGINATION_CLASS": "open_mic.pagination.CustomPagination",
+    "PAGE_SIZE": 10,
+    "ORDERING_PARAM": "order",
+}
+
+CORS_ORIGIN_WHITELIST = [
+     'http://localhost:3000'
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_METHODS = ["DELETE", "GET", "OPTIONS", "PATCH", "POST", "PUT"]
+
+PAGE_SIZE_WITHOUT_PAGINATION = 100
+PAGE_SIZE_WITH_PAGINATION = 10
+
+
+# AWS S3 config
+AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY", "")
+AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME", "")
+AWS_STORAGE_REGION = config("AWS_STORAGE_REGION", "")
+
+USE_S3 = (
+        AWS_ACCESS_KEY_ID and
+        AWS_SECRET_ACCESS_KEY and
+        AWS_STORAGE_BUCKET_NAME and
+        AWS_STORAGE_REGION
+)
+
+if USE_S3:
+    AWS_S3_CUSTOM_DOMAIN = config("AWS_S3_CUSTOM_DOMAIN", "")
+    AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
+    AWS_DEFAULT_ACL = config("AWS_DEFAULT_ACL", "public-read")
+    AWS_MEDIA_LOCATION = config("AWS_MEDIA_LOCATION", "media")
+    AWS_AUTO_CREATE_BUCKET = config("AWS_AUTO_CREATE_BUCKET", True)
+    AWS_QUERYSTRING_AUTH = False
+    DEFAULT_FILE_STORAGE = config("DEFAULT_FILE_STORAGE", "home.storage_backends.MediaStorage")
